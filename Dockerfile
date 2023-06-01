47a0642c21d1c1431b6f38d2a9c8fc94c51c21f7
Config_Report_Demo <leo_hardy@hotmail.co.uk>
2023-06-01 10:47:49 +0100

    removed docker for test build

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# syntax=docker/dockerfile:1
-FROM node:18-alpine
-WORKDIR /app
-COPY ./package.json ./
-RUN npm i
-COPY . .
-RUN npm run build
-CMD [ "npm", "run", "start" ]
---
+++ build/precache-manifest.5371c07f572a3c577c58d173470cd129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15364c132d7dd164b541",
+    "url": "./static/css/main.9a98d104.chunk.css"
+  },
+  {
+    "revision": "15364c132d7dd164b541",
+    "url": "./static/js/main.d00cc78e.chunk.js"
+  },
+  {
+    "revision": "e45f3684a301e2710a40",
+    "url": "./static/js/runtime~main.4824c0af.js"
+  },
+  {
+    "revision": "d6b89d5a2792580cf0ea",
+    "url": "./static/js/2.7d23f630.chunk.js"
+  },
+  {
+    "revision": "067283099e8f299e405e",
+    "url": "./static/js/3.13768eaf.chunk.js"
+  },
+  {
+    "revision": "8296bd066ad05afd4eb029e6785f2eb2",
+    "url": "./index.html"
+  }
+];
